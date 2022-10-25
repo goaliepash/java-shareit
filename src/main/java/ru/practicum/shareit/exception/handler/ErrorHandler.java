068328d568588fa17ab3a273ba481f3c2d243681
goaliepash <goaltenders@yandex.ru>
2022-10-25 16:52:34 +0300

    refactor: обработка Throwable, MethodArgumentNotValidException и ConstraintViolationException

--- src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/handler/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.exception.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.shareit.exception.ItemForbiddenException;
@@ -12,12 +13,38 @@ import ru.practicum.shareit.exception.UserNotFoundException;
 import ru.practicum.shareit.item.ItemController;
 import ru.practicum.shareit.user.UserController;
 
+import javax.validation.ConstraintViolationException;
+
 @Slf4j
 @RestControllerAdvice(assignableTypes = {UserController.class, ItemController.class})
 public class ErrorHandler {
 
     @ExceptionHandler
+    private ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        log.info(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException exception) {
+        log.info(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<String> handleItemForbiddenException(ItemForbiddenException exception) {
+        log.info(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException exception) {
+        log.info(exception.getMessage());
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<String> handleItemNotFoundException(ItemNotFoundException exception) {
         log.info(exception.getMessage());
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
@@ -29,14 +56,8 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
-    private ResponseEntity<String> handleItemNotFoundException(ItemNotFoundException exception) {
-        log.info(exception.getMessage());
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    private ResponseEntity<String> handleItemForbiddenException(ItemForbiddenException exception) {
-        log.info(exception.getMessage());
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.FORBIDDEN);
+    public ResponseEntity<String> handleThrowable(Throwable throwable) {
+        log.info(throwable.getMessage());
+        return new ResponseEntity<>(throwable.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
